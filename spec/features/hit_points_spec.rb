4985fdbc5bb0c87f126ed5bf106e412686e36d4d
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-22 21:17:42 +0100

    Test and logic for Player 2 being able to see his hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'View hit points' do
     sign_in_and_play
     expect(page).to have_content 'Yuliya: 60 HP'
   end
+
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Ben: 60 HP'
+  end
 end
